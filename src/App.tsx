b97f8b327762e4c0388dae40f44540fef539b946
ejk9 <46539941+ejk9@users.noreply.github.com>
2021-05-13 16:00:59 -0600

    Navbar created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <Navbar />
+    
+  );
+}
+
+export default App;
